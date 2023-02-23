5538d388f85fd5457b30787efbe93a5f99534bd0
AngGarcia <94858449+AngGarcia@users.noreply.github.com>
2023-02-23 14:25:49 +0100

    Added react to project
    Added "Activity" component
    Added "Button" component
    
    Co-Authored-By: Joaquín Bazterrica <joaquin.bazterrica@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Activity from './components/Activity'
+
+function App() {
+  return (
+    <div className="App">
+      <Activity/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Activity.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Activity() {
+    return (
+        //Title, photo, description(modality, category), schedule, location, subscription_button
+        <div></div>
+    )
+}
+
+export default Activity;
