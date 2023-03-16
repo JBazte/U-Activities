7b32a3210a4300fe1abda4a0b4a7e8c4b2fffa47
Suli25 <saulit25@gmail.com>
2023-03-16 14:27:27 +0100

    All tables files created but empty xD
    The "app\models\index.js" is also updated

--- app/models/index.js
+++ app/models/index.js
@@ -21,6 +21,11 @@ db.sequelize = sequelize;
 
 
 //tablas
+db.administrators = require("./administrators.model.js")(sequelize, Sequelize);
 db.members = require("./members.model.js")(sequelize, Sequelize);
+db.preferences = require("./preferences.model.js")(sequelize, Sequelize);
+db.sponsors = require("./sponsors.model.js")(sequelize, Sequelize);
+db.activities = require("./activities.model.js")(sequelize, Sequelize);
+db.participation = require("./participation.model.js")(sequelize, Sequelize);
 
 module.exports = db;
