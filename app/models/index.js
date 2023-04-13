9079e3ee9a9ba01d1d17e405e3b4b4a328423a6a
Suli25 <saulit25@gmail.com>
2023-04-13 14:00:20 +0200

    All models working
    All the models can be automactly created with no problem in the "mysql" server.
    Server connection OK,
    Tables created OK
    
    Co-Authored-By: AlejandroEscuderoMatas <94743762+AlejandroEscuderoMatas@users.noreply.github.com>
    Co-Authored-By: Marcostiz <73120678+Marcostiz@users.noreply.github.com>

--- app/models/index.js
+++ app/models/index.js
@@ -15,19 +15,19 @@ db.sequelize = sequelize;
 
 
 //tablas
-//db.administrators = require("./administrators.model.js")(sequelize, DataTypes);
+db.administrators = require("./administrators.model.js")(sequelize, DataTypes);
 db.members = require("./members.model.js")(sequelize, DataTypes);
 db.preferences = require("./preferences.model.js")(sequelize, DataTypes);
-//db.sponsors = require("./sponsors.model.js")(sequelize, DataTypes);
-//db.activities = require("./activities.model.js")(sequelize, DataTypes);
-//db.participation = require("./participation.model.js")(sequelize, DataTypes);
+db.sponsors = require("./sponsors.model.js")(sequelize, DataTypes);
+db.activities = require("./activities.model.js")(sequelize, DataTypes);
+db.participation = require("./participation.model.js")(sequelize, DataTypes);
 
 const Members = db.members
 const Preferences = db.preferences
-//const Administrators = db.administrators
-//const Sponsors = db.sponsors
-//const Activities = db.activities
-//const Participation = db.participation
+const Administrators = db.administrators
+const Sponsors = db.sponsors
+const Activities = db.activities
+const Participation = db.participation
 
 //ASIGNAMOS FK
 //RELACION 1-N member y preferences
@@ -40,30 +40,43 @@ Preferences.belongsTo(Members,{
   targetId:'id'
 });
 
-/*
+
 //RELACION 1-N sponsor y activities
 Sponsors.hasMany(Activities, {
-  foreignKey: 'sponsor_id'
+  foreignKey: 'sponsor_id',
+  sourceKey:'id'
 });
-Activities.belongsTo(Sponsors);
+Activities.belongsTo(Sponsors, {
+  foreignKey:'sponsor_id',
+  targetId:'id'
+});
 
 
 //RELACION N-N entre members y activities, tabla partcipation
 Members.hasMany(Participation, {
-  foreignKey: 'member_id'
+  foreignKey: 'member_id',
+  sourceKey: 'id'
 });
-Participation.belongsTo(Members);
+Participation.belongsTo(Members,{
+  foreignKey:'member_id',
+  targetId:'id'
+});
 
 Activities.hasMany(Participation, {
-  foreignKey: 'activity_id'
+  foreignKey: 'activity_id',
+  sourceKey:'id'
 });
-Participation.belongsTo(Activities);
+Participation.belongsTo(Activities, {
+  foreignKey:'activity_id',
+  targetId:'id'
+});
 
-*/
 
+
+//Sincronizamos las tablsa en la base de datos
 db.sequelize.sync({force: true })
     .then(() => {
         console.log('Tablas sincronizadas')
-    })
+    });
 
 module.exports = db;
