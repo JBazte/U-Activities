575badeb6ba1eb538ff757da16a01d3ae2e22186
AlejandroEscuderoMatas <alex.escudero1100@gmail.com>
2023-04-17 14:33:47 +0200

    Validators for users and admins
    We have to solve an error

--- app/validators/members.validators.js
+++ app/validators/members.validators.js
@@ -1,11 +1,16 @@
+
 const { check } = require("express-validator")
-const validateResults = require("../utils/handleValidator")
+const validateResults = require("../utils/handleValidators")
 
 const validatorCreateMember = [
-    check("first_name").exists().notEmpty(),
-    check("last_name").exists().notEmpty(),
-    check("email").exists().notEmpty().isEmail,
+    check("first_name").exists().notEmpty().isAlpha()/*withMessage("Ha habido un problema al introducir el nombre")*/,
+    check("last_name").exists().notEmpty().isAlpha(),
+    check("email").exists().notEmpty().isEmail(),
+    check("bith_date").exists().notEmpty()/*.isDate()*/,
+    check("dni").exists().notEmpty().isAlphanumeric(),
+    check("genre").exists().notEmpty().isAlpha(),
     check("password").exists().notEmpty(),
+    check("studies").exists().notEmpty().isAlpha(),
     check("phone").exists().notEmpty(),
 
     //Middleware tiene que responder después de la petición
@@ -16,8 +21,10 @@ const validatorCreateMember = [
 ]
 
 const validatorGetMember = [
-    check("id").exists().notEmpty().isMongoId(),
+    check("id").exists().notEmpty(),
     (req, res, next) => {
         return validateResults(req, res, next)
     }
 ]
+
+module.exports = { validatorCreateMember, validatorGetMember }
