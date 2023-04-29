f058b2e9679949452a9707ef009dfa12a6044234
AlejandroEscuderoMatas <alex.escudero1100@gmail.com>
2023-04-29 10:50:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require('cors');
-require('dotenv').config();
+
 const bodyParser = require("body-parser");
 
 const app = express();
