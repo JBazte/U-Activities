af82636f39bff91d1ec84e2f9a4b2ea8ca80c2df
AlejandroEscuderoMatas <alex.escudero1100@gmail.com>
2023-05-04 14:28:27 +0200

    Check tokens for admins and sponsors

--- app/middleware/session.js
+++ app/middleware/session.js
@@ -1,6 +1,6 @@
 const { handleHttpError } = require("../utils/handleErrors")
 const { verifyTokenUser }     = require("../utils/handleJwt")
-const { members }      = require("../models")
+const { members, sponsors, administrators }      = require("../models")
 
 const authMiddlewareMember = async (req, res, next) => {
     try{
@@ -30,4 +30,60 @@ const authMiddlewareMember = async (req, res, next) => {
     }
 }
 
-module.exports = { authMiddlewareMember }
+const authMiddlewareSponsor = async (req, res, next) => {
+    try{
+        if (!req.headers.authorization) {
+            handleHttpError(res, "NOT_TOKEN", 401)
+            return
+        }
+
+        // Nos llega la palabra reservada Bearer (es un estándar) y el Token, así que me quedo con la última parte
+        const token = req.headers.authorization.split(' ').pop() 
+        //Del token, miramos en Payload (revisar verifyToken de utils/handleJwt)
+        const dataToken = await verifyTokenUser(token)
+
+        if(!dataToken) {
+            handleHttpError(res, "NOT_PAYLOAD_DATA", 401)
+            return
+        }
+
+        const sponsor = await sponsors.findByPk(dataToken.id)
+        req.sponsor = sponsor // Inyecto al user en la petición
+        
+        next()
+
+    }catch(err){
+        console.log(err)
+        handleHttpError(res, "NOT_SPONSOR_SESSION", 401)
+    }
+}
+
+const authMiddlewareAdministrator = async (req, res, next) => {
+    try{
+        if (!req.headers.authorization) {
+            handleHttpError(res, "NOT_TOKEN", 401)
+            return
+        }
+
+        // Nos llega la palabra reservada Bearer (es un estándar) y el Token, así que me quedo con la última parte
+        const token = req.headers.authorization.split(' ').pop() 
+        //Del token, miramos en Payload (revisar verifyToken de utils/handleJwt)
+        const dataToken = await verifyTokenUser(token)
+
+        if(!dataToken) {
+            handleHttpError(res, "NOT_PAYLOAD_DATA", 401)
+            return
+        }
+
+        const admin = await administrators.findByPk(dataToken.id)
+        req.admin = admin // Inyecto al user en la petición
+        
+        next()
+
+    }catch(err){
+        console.log(err)
+        handleHttpError(res, "NOT_ADINISTRATOR_SESSION", 401)
+    }
+}
+
+module.exports = { authMiddlewareMember, authMiddlewareSponsor, authMiddlewareAdministrator }
