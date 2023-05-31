60d0e196e45c6f16caa5ebca2c196d1a9570daea
Marcostiz <73120678+Marcostiz@users.noreply.github.com>
2023-05-31 15:56:39 +0200

    removed more functions from swagger

--- app/routes/members.routes.js
+++ app/routes/members.routes.js
@@ -9,75 +9,12 @@ module.exports = app => {
   
   router.post("/", validatorCreateMember, members.create);
   
-  /**
-   * @openapi
-   * /api/members:
-   *  get:
-   *      tags:
-   *      - members
-   *      summary: Get members in the System
-   *      description: ''
-   *      responses:
-   *          '200':
-   *              description: Returns the members
-   *          '500':
-   *              description: Server error
-   */
   router.get("/", members.findAll);
 
-  /**
-   * @openapi
-   * /api/members/{id}:
-   *  get:
-   *      tags:
-   *      - Members
-   *      summary: Get a member by id in the System
-   *      description: ''
-   *      parameters:
-   *          -   name: id
-   *              in: path
-   *              description: id that is requested
-   *              required: true
-   *              schema:
-   *                  type: string
-   *      responses:
-   *          '200':
-   *              description: Returns the member
-   *          '500':
-   *              description: Server error
-   */
   router.get("/:id", validatorGetMember, members.findOne);
 
   router.get("/sponsor/:sponsor_id", validatorGetActivitySponsor, activities.getActivities);
 
-  /**
-   * @openapi
-   * /api/members/{id}:
-   *  put:
-   *      tags:
-   *      - Members
-   *      summary: Update a member
-   *      description: Update a member, a member will just be able to edit his own account, as he will get the id from the token
-   *      parameters:
-   *          -   name: id
-   *              in: path
-   *              description: id that need to be updated
-   *              required: true
-   *              schema:
-   *                  type: string
-   *      requestBody:
-   *          content:
-   *              application/json:
-   *                  schema:
-   *                      $ref: "#/components/schemas/members"
-   *      responses:
-   *          '200':
-   *              description: Returns the inserted object
-   *          '401':
-   *              description: Validation error
-   *      security:
-   *          - bearerAuth: []
-   */
   router.put("/", authMiddlewareMember, hashPasswordUpdate, members.update)
 
   router.delete("/:id", validatorGetMember, members.deleteOne);
--- app/routes/participation.routes.js
+++ app/routes/participation.routes.js
@@ -5,27 +5,6 @@ module.exports = app => {
     const {validatorCreateParticipation, validatorGetParticipation, validatorGetActivityParticipation, checkValidActivity} = require("../validators/participation.validators.js")
   
     var router = require("express").Router();
-    /**
-    * @openapi
-    * /api/participation:
-    *  post:
-    *      tags:
-    *      - Participation
-    *      summary: create a new member
-    *      description: ''
-    *      requestBody:
-    *          content:
-    *              application/json:
-    *                  schema:
-    *                      $ref: "#/components/schemas/participation"
-    *      responses:
-    *          '200':
-    *              description: Returns the inserted object
-    *          '401':
-    *              description: Validation error
-    *      security:
-    *          - bearerAuth: []
-    */
     router.post("/:activity_id", authMiddlewareMember, checkValidActivity,  participation.create);
 
     router.get("/", participation.findAll);
@@ -36,29 +15,6 @@ module.exports = app => {
     
     router.put("/:id", validatorGetParticipation, validatorCreateParticipation, participation.update);
     
-    /**
-     * @openapi
-     * /api/participation/{id}:
-     *  delete:
-     *      tags:
-     *      - Members
-     *      summary: Delete a participation
-     *      description: Delete a participation by an id
-     *      parameters:
-     *          -   name: id
-     *              in: path
-     *              description: ''
-     *              required: true
-     *              schema:
-     *                  type: string
-     *      responses:
-     *          '200':
-     *              description: Returns the status
-     *          '401':
-     *              description: Validation error
-     *      security:
-     *          - bearerAuth: []
-     */
     router.delete("/:activity_id", authMiddlewareMember, validatorGetParticipation, participation.deleteOne);
 
     app.use('/api/participation', router);
