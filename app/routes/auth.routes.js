9ee4ec28fad647848c47509c8aee9e0cfeb2d976
AlejandroEscuderoMatas <alex.escudero1100@gmail.com>
2023-05-31 15:14:44 +0200

    Documentation of auth routes

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -12,21 +12,112 @@ module.exports = app => {
     var router = express.Router();
 
     //LOGIN UN MEMBER
+    /**
+     * @openapi
+     * /api/auth/login/member:
+     *  post:
+     *      tags:
+     *      - Registered User
+     *      summary: Login to your user
+     *      description: ''
+     *      responses:
+     *          '200':
+     *              description: Get your user
+     *          '403':
+     *              description: Errors in parameters or not found user
+     *          '500':
+     *              description: Server error
+     */
     router.post("/login/member", validatorLoginMember, loginMember)
 
     //REGISTRAR UN MEMBER
+    /**
+     * @openapi
+     * /api/auth/register/member:
+     *  post:
+     *      tags:
+     *      - Not registered User
+     *      summary: Create a user
+     *      description: ''
+     *      responses:
+     *          '200':
+     *              description: Get the user edited
+     *          '403':
+     *              description: Errors in parameters or already used uniques
+     *          '500':
+     *              description: Server error
+     */
     router.post("/register/member", validatorRegisterMember, registerMember, preferences.create)
 
     //REGISTAR UN ADMIN
     router.post("/register/admin", validatorCreateAdministrator, registerAdmin)
 
     //LOGIN DEL SPONSOR
+    /**
+     * @openapi
+     * /api/auth/login/sponsor:
+     *  post:
+     *      tags:
+     *      - Sponsor
+     *      summary: Login to your sponsor
+     *      description: ''
+     *      responses:
+     *          '200':
+     *              description: Get your sponsor
+     *          '403':
+     *              description: Errors in parameters or not found sponsor
+     *          '500':
+     *              description: Server error
+     */
     router.post("/login/sponsor", validatorLoginSponsor, loginSponsor)
 
     //REGISTAR UN SPONSOR
+    /**
+     * @openapi
+     * /api/auth/register/sponsor:
+     *  post:
+     *      tags:
+     *      - Administrator
+     *      summary: An admin registers a sponsor
+     *      description: 'A verified jwt admin registers a sponsor in db, which will return the jwt from sponsor'
+     *      responses:
+     *          '200':
+     *              description: Register a sponsor
+     *          '403':
+     *              description: Errors in parameters or invalid parameters of sponsor
+     *          '500':
+     *              description: Server error
+     *      security:
+     *          - bearerAuth: []
+     */
     router.post("/register/sponsor", authMiddlewareAdministrator, validatorCreateSponsor, registerSponsor)
 
     //DAR DE BAJA UN SPONSOR
+    /**
+     * @openapi
+     * /api/auth/delete/sponsor/:id:
+     *  delete:
+     *      tags:
+     *      - Sponsor
+     *      summary: An sponsor deletes his own sponsor
+     *      description: 'A verified jwt admin registers a sponsor in db, which will return the jwt from sponsor'
+     *      parameters:
+    *          -   name: id
+    *              in: path
+    *              description: id that need to be updated
+    *              required: true
+    *              schema:
+    *                  type: string
+     *      responses:
+     *          '200':
+     *              description: Delete a sponsor
+     *          '403':
+     *              description: Errors in parameters or not found sponsor
+     *          '500':
+     *              description: Server error
+     *      security:
+     *          - bearerAuth: []
+     */
     router.delete("/delete/sponsor/:id", authMiddlewareAdministrator, validatorGetSponsor, sponsors.deleteOne)
 
     //Un sponsor deberá tener acceso a los usuarios inscritos a una actividad
