487275a5ddcb1f144ef7e28111df1c308f3ca32b
Joaquin Bazterrica <joaquin.bazterrica@gmail.com>
2023-06-02 00:28:32 +0200

    Added Error Messages on forms & Cookies

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import NotificationContainer from '../components/NotificationContainer'
 import Carousel from '../components/Carousel';
 
 function Notifications() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const userCookie = document.cookie.split(';').find((cookie) => cookie.trim().startsWith('user-token='));
+        const adminCookie = document.cookie.split(';').find((cookie) => cookie.trim().startsWith('admin-token='));
+        const sponsorCookie = document.cookie.split(';').find((cookie) => cookie.trim().startsWith('sponsor-token='));
+
+        if (!userCookie && !adminCookie && !sponsorCookie) {
+            navigate('/login');
+        }
+    }, [navigate]);
+
     return (
         <>
             <Navbar />
